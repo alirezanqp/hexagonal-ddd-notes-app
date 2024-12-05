5fb271b2539a2f7e7972bbbd4db1f9b349b525c4
Alireza Naghdipour <alirezanqp@gmail.com>
2024-12-06 00:11:09 +0330

    feat(UserAccount): add user account module
    This commit introduces a new module for user account management. It includes:
    
    - User registration and login use cases
    - MongoDB user repository
    - User mapper
    - User HTTP controller
    - Bcrypt hash implementation
    - JWT token implementation
    - Mongoose module for database interaction
    - JWT module for token generation and validation
    
    This module provides the necessary infrastructure for managing user accounts, including registration, login, and authentication.

---
+++ src/UserAccount/user-account.module.ts
@@ -0,0 +1,43 @@
+import { Module, Provider } from '@nestjs/common';
+import { RegisterUserUseCase } from './application/use-cases/commands/register-user/register-user.usecase';
+import { LoginUserUseCase } from './application/use-cases/commands/login-user/login-user.usecase';
+import { UserRepository } from './application/ports/user.repository.port';
+import { MongodbUserRepository } from './infrastructure/output/persistence/mongodb.user.repository';
+import { UserMapper } from './infrastructure/output/persistence/mapper/user.mapper';
+import { UserHttpController } from './infrastructure/input/http/user.http.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import {
+  UserCollectionName,
+  UserPersistenceSchema,
+} from './infrastructure/output/persistence/model/user.persistence.model';
+import { Hash } from './application/ports/hash.port';
+import { BcryptHash } from './infrastructure/output/bcrypt.hash';
+import { Token } from './application/ports/token.port';
+import { JwtToken } from './infrastructure/output/jwt.token';
+import { JwtModule } from '@nestjs/jwt';
+
+const commands = [RegisterUserUseCase, LoginUserUseCase];
+
+const infrastructure: Provider[] = [
+  { provide: UserRepository, useClass: MongodbUserRepository },
+  { provide: Hash, useClass: BcryptHash },
+  { provide: Token, useClass: JwtToken },
+];
+
+const mapper = [UserMapper];
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: UserCollectionName, schema: UserPersistenceSchema },
+    ]),
+    JwtModule.register({
+      secret: 'super-secret',
+      global: true,
+      signOptions: { expiresIn: '1d' },
+    }),
+  ],
+  controllers: [UserHttpController],
+  providers: [...commands, ...infrastructure, ...mapper],
+})
+export class UserAccountModule {}
