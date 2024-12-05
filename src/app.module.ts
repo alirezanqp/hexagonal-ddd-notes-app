6de93ae91f1ec29210b8c9b7737c3abe1d9065b5
Alireza Naghdipour <alirezanqp@gmail.com>
2024-12-06 00:11:59 +0330

    feat(app.module.ts): add mongoose, request context, event emitter and user account modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,24 @@
-import { Module } from '@nestjs/common';
+import { Module, Provider } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { RequestContextModule } from 'nestjs-request-context';
+import { NestjsEventEmitterModule } from '@common/infrastructure/event-publisher/nest-event-emitter.publisher.module';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { ContextInterceptor } from '@common/infrastructure/request-context/context.interceptor';
+import { UserAccountModule } from './UserAccount/user-account.module';
+
+const interceptors: Provider[] = [
+  { provide: APP_INTERCEPTOR, useClass: ContextInterceptor },
+];
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [
+    MongooseModule.forRoot(''),
+    RequestContextModule,
+    NestjsEventEmitterModule,
+
+    // Domain Modules
+    UserAccountModule,
+  ],
+  providers: [...interceptors],
 })
 export class AppModule {}
